69667db8254b68023ae902fa3b0c7b07e0421dab
Nicholas Rising <nicholas.rising@gmail.com>
2021-10-03 17:49:23 -0400

    Create Card class and add horizontal cards

--- cards.py
+++ cards.py
@@ -2,123 +2,225 @@ import os
 import random
 import time
 
+WIDTH=os.get_terminal_size().columns
+HEIGHT=os.get_terminal_size().lines-1
+
 SPADE=SPADES='\u2660'
 DIAMOND=DIAMONDS='\u2666'
 CLUB=CLUBS='\u2663'
 HEART=HEARTS='\u2665'
 
-def newDeck():
-    deck=[(rank,suit) for rank in ['A','2','3','4','5','6','7','8','9','10','J','Q','K'] for suit in [SPADES,DIAMONDS,CLUBS,HEARTS]]
-    random.shuffle(deck)
-    return deck
+class Card:
+    def __init__(self,rank=None,suit=None,x=0,y=0,isHorizontal=False):
+        self.rank=rank
+        self.suit=suit
+        self.x=x
+        self.y=y
+        self.isHorizontal=isHorizontal
 
-def cardGrid(card):
-    grid=[f'+{"-"*9}+']
-    if card:
-        grid+=[f'|{card[0]}{" "*(8-len(card[0]))}{card[1]}|']
-        if card[0]=='A':
-            grid+=[f'|{" "*9}|']
-            grid+=[f'|{" "*9}|']
-            grid+=[f'|{" "*4}{card[1]}{" "*4}|']
-            grid+=[f'|{" "*9}|']
-            grid+=[f'|{" "*9}|']
-        elif card[0]=='2':
-            grid+=[f'|{" "*4}{card[1]}{" "*4}|']
-            grid+=[f'|{" "*9}|']
-            grid+=[f'|{" "*9}|']
-            grid+=[f'|{" "*9}|']
-            grid+=[f'|{" "*4}{card[1]}{" "*4}|']
-        elif card[0]=='3':
-            grid+=[f'|{" "*4}{card[1]}{" "*4}|']
-            grid+=[f'|{" "*9}|']
-            grid+=[f'|{" "*4}{card[1]}{" "*4}|']
-            grid+=[f'|{" "*9}|']
-            grid+=[f'|{" "*4}{card[1]}{" "*4}|']
-        elif card[0]=='4':
-            grid+=[f'|{" "*3}{card[1]} {card[1]}{" "*3}|']
-            grid+=[f'|{" "*9}|']
-            grid+=[f'|{" "*9}|']
-            grid+=[f'|{" "*9}|']
-            grid+=[f'|{" "*3}{card[1]} {card[1]}{" "*3}|']
-        elif card[0]=='5':
-            grid+=[f'|{" "*3}{card[1]} {card[1]}{" "*3}|']
-            grid+=[f'|{" "*9}|']
-            grid+=[f'|{" "*4}{card[1]}{" "*4}|']
-            grid+=[f'|{" "*9}|']
-            grid+=[f'|{" "*3}{card[1]} {card[1]}{" "*3}|']
-        elif card[0]=='6':
-            grid+=[f'|{" "*3}{card[1]} {card[1]}{" "*3}|']
-            grid+=[f'|{" "*9}|']
-            grid+=[f'|{" "*3}{card[1]} {card[1]}{" "*3}|']
-            grid+=[f'|{" "*9}|']
-            grid+=[f'|{" "*3}{card[1]} {card[1]}{" "*3}|']
-        elif card[0]=='7':
-            grid+=[f'|{" "*3}{card[1]} {card[1]}{" "*3}|']
-            grid+=[f'|{" "*4}{card[1]}{" "*4}|']
-            grid+=[f'|{" "*3}{card[1]} {card[1]}{" "*3}|']
-            grid+=[f'|{" "*9}|']
-            grid+=[f'|{" "*3}{card[1]} {card[1]}{" "*3}|']
-        elif card[0]=='8':
-            grid+=[f'|{" "*3}{card[1]} {card[1]}{" "*3}|']
-            grid+=[f'|{" "*4}{card[1]}{" "*4}|']
-            grid+=[f'|{" "*3}{card[1]} {card[1]}{" "*3}|']
-            grid+=[f'|{" "*4}{card[1]}{" "*4}|']
-            grid+=[f'|{" "*3}{card[1]} {card[1]}{" "*3}|']
-        elif card[0]=='9':
-            grid+=[f'|{" "*3}{card[1]} {card[1]}{" "*3}|']
-            grid+=[f'|{" "*3}{card[1]} {card[1]}{" "*3}|']
-            grid+=[f'|{" "*4}{card[1]}{" "*4}|']
-            grid+=[f'|{" "*3}{card[1]} {card[1]}{" "*3}|']
-            grid+=[f'|{" "*3}{card[1]} {card[1]}{" "*3}|']
-        elif card[0]=='10':
-            grid+=[f'|{" "*3}{card[1]} {card[1]}{" "*3}|']
-            grid+=[f'|{" "*3}{card[1]} {card[1]}{" "*3}|']
-            grid+=[f'|{" "*3}{card[1]} {card[1]}{" "*3}|']
-            grid+=[f'|{" "*3}{card[1]} {card[1]}{" "*3}|']
-            grid+=[f'|{" "*3}{card[1]} {card[1]}{" "*3}|']
-        elif card[0]=='J':
-            grid+=[f'|    ww   |']
-            grid+=[f'|    {{)   |']
-            grid+=[f'|    {card[1]}    |']
-            grid+=[f'|    {card[1]}    |']
-            grid+=[f'|   {card[1]}{card[1]}[   |']
-        elif card[0]=='Q':
-            grid+=[f'|    ww   |']
-            grid+=[f'|    {{(   |']
-            grid+=[f'|    {card[1]}{card[1]}   |']
-            grid+=[f'|   {card[1]}{card[1]}{card[1]}   |']
-            grid+=[f'|   {card[1]}{card[1]}O   |']
-        elif card[0]=='K':
-            grid+=[f'|    WW   |']
-            grid+=[f'|    {{)   |']
-            grid+=[f'|    {card[1]}{card[1]}   |']
-            grid+=[f'|   {card[1]}{card[1]}{card[1]}   |']
-            grid+=[f'|   {card[1]}{card[1]}>   |']
-        grid+=[f'|{card[1]}{" "*(8-len(card[0]))}{card[0]}|']
-    else:
-        grid+=[f'|{" "*9}|']
-        grid+=[f'|  +---+  |']
-        grid+=[f'|  |   |  |']
-        grid+=[f'|  | O |  |']
-        grid+=[f'|  |   |  |']
-        grid+=[f'|  +---+  |']
-        grid+=[f'|{" "*9}|']
-    grid+=[f'+{"-"*9}+']
-    return [list(line) for line in grid]
+    def getCardGrid(self):
+        if not self.isHorizontal:
+            grid=[f'+{"-"*9}+']
+            if self.rank and self.suit:
+                grid+=[f'|{self.rank}{" "*(8-len(self.rank))}{self.suit}|']
+                if self.rank=='A':
+                    grid+=[f'|{" "*9}|']
+                    grid+=[f'|{" "*9}|']
+                    grid+=[f'|    {self.suit}    |']
+                    grid+=[f'|{" "*9}|']
+                    grid+=[f'|{" "*9}|']
+                elif self.rank=='2':
+                    grid+=[f'|    {self.suit}    |']
+                    grid+=[f'|{" "*9}|']
+                    grid+=[f'|{" "*9}|']
+                    grid+=[f'|{" "*9}|']
+                    grid+=[f'|    {self.suit}    |']
+                elif self.rank=='3':
+                    grid+=[f'|    {self.suit}    |']
+                    grid+=[f'|{" "*9}|']
+                    grid+=[f'|    {self.suit}    |']
+                    grid+=[f'|{" "*9}|']
+                    grid+=[f'|    {self.suit}    |']
+                elif self.rank=='4':
+                    grid+=[f'|   {self.suit} {self.suit}   |']
+                    grid+=[f'|{" "*9}|']
+                    grid+=[f'|{" "*9}|']
+                    grid+=[f'|{" "*9}|']
+                    grid+=[f'|   {self.suit} {self.suit}   |']
+                elif self.rank=='5':
+                    grid+=[f'|   {self.suit} {self.suit}   |']
+                    grid+=[f'|{" "*9}|']
+                    grid+=[f'|    {self.suit}    |']
+                    grid+=[f'|{" "*9}|']
+                    grid+=[f'|   {self.suit} {self.suit}   |']
+                elif self.rank=='6':
+                    grid+=[f'|   {self.suit} {self.suit}   |']
+                    grid+=[f'|{" "*9}|']
+                    grid+=[f'|   {self.suit} {self.suit}   |']
+                    grid+=[f'|{" "*9}|']
+                    grid+=[f'|   {self.suit} {self.suit}   |']
+                elif self.rank=='7':
+                    grid+=[f'|   {self.suit} {self.suit}   |']
+                    grid+=[f'|    {self.suit}    |']
+                    grid+=[f'|   {self.suit} {self.suit}   |']
+                    grid+=[f'|{" "*9}|']
+                    grid+=[f'|   {self.suit} {self.suit}   |']
+                elif self.rank=='8':
+                    grid+=[f'|   {self.suit} {self.suit}   |']
+                    grid+=[f'|    {self.suit}    |']
+                    grid+=[f'|   {self.suit} {self.suit}   |']
+                    grid+=[f'|    {self.suit}    |']
+                    grid+=[f'|   {self.suit} {self.suit}   |']
+                elif self.rank=='9':
+                    grid+=[f'|   {self.suit} {self.suit}   |']
+                    grid+=[f'|   {self.suit} {self.suit}   |']
+                    grid+=[f'|    {self.suit}    |']
+                    grid+=[f'|   {self.suit} {self.suit}   |']
+                    grid+=[f'|   {self.suit} {self.suit}   |']
+                elif self.rank=='10':
+                    grid+=[f'|   {self.suit} {self.suit}   |']
+                    grid+=[f'|   {self.suit} {self.suit}   |']
+                    grid+=[f'|   {self.suit} {self.suit}   |']
+                    grid+=[f'|   {self.suit} {self.suit}   |']
+                    grid+=[f'|   {self.suit} {self.suit}   |']
+                elif self.rank=='J':
+                    grid+=[f'|    ww   |']
+                    grid+=[f'|    {{)   |']
+                    grid+=[f'|    {self.suit}    |']
+                    grid+=[f'|    {self.suit}    |']
+                    grid+=[f'|   {self.suit}{self.suit}[   |']
+                elif self.rank=='Q':
+                    grid+=[f'|    ww   |']
+                    grid+=[f'|    {{(   |']
+                    grid+=[f'|    {self.suit}{self.suit}   |']
+                    grid+=[f'|   {self.suit}{self.suit}{self.suit}   |']
+                    grid+=[f'|   {self.suit}{self.suit}O   |']
+                elif self.rank=='K':
+                    grid+=[f'|    WW   |']
+                    grid+=[f'|    {{)   |']
+                    grid+=[f'|    {self.suit}{self.suit}   |']
+                    grid+=[f'|   {self.suit}{self.suit}{self.suit}   |']
+                    grid+=[f'|   {self.suit}{self.suit}>   |']
+                grid+=[f'|{self.suit}{" "*(8-len(self.rank))}{self.rank}|']
+            else:
+                grid+=[f'|{" "*9}|']
+                grid+=[f'|  +---+  |']
+                grid+=[f'|  |   |  |']
+                grid+=[f'|  | O |  |']
+                grid+=[f'|  |   |  |']
+                grid+=[f'|  +---+  |']
+                grid+=[f'|{" "*9}|']
+            grid+=[f'+{"-"*9}+']
+        else:
+            
+# +-------------------+
+# |♥                 2|
+# |    ♥         ♥    |
+# |2                 ♥|
+# +-------------------+
+            grid=[f'+{"-"*15}+']
+            if self.rank and self.suit:
+                if self.rank!='J' and self.rank!='Q' and self.rank!='K':
+                    grid+=[f'|{self.suit}{" "*(14-len(self.rank))}{self.rank}|']
+                    if self.rank=='A':
+                        grid+=[f'|{" "*15}|']
+                        grid+=[f'|       {self.suit}       |']
+                        grid+=[f'|{" "*15}|']
+                    elif self.rank=='2':
+                        grid+=[f'|{" "*15}|']
+                        grid+=[f'|   {self.suit}       {self.suit}   |']
+                        grid+=[f'|{" "*15}|']
+                    elif self.rank=='3':
+                        grid+=[f'|{" "*15}|']
+                        grid+=[f'|   {self.suit}   {self.suit}   {self.suit}   |']
+                        grid+=[f'|{" "*15}|']
+                    elif self.rank=='4':
+                        grid+=[f'|   {self.suit}       {self.suit}   |']
+                        grid+=[f'|{" "*15}|']
+                        grid+=[f'|   {self.suit}       {self.suit}   |']
+                    elif self.rank=='5':
+                        grid+=[f'|   {self.suit}       {self.suit}   |']
+                        grid+=[f'|       {self.suit}       |']
+                        grid+=[f'|   {self.suit}       {self.suit}   |']
+                    elif self.rank=='6':
+                        grid+=[f'|   {self.suit}   {self.suit}   {self.suit}   |']
+                        grid+=[f'|{" "*15}|']
+                        grid+=[f'|   {self.suit}   {self.suit}   {self.suit}   |']
+                    elif self.rank=='7':
+                        grid+=[f'|   {self.suit}   {self.suit}   {self.suit}   |']
+                        grid+=[f'|     {self.suit}{" "*9}|']
+                        grid+=[f'|   {self.suit}   {self.suit}   {self.suit}   |']
+                    elif self.rank=='8':
+                        grid+=[f'|   {self.suit}   {self.suit}   {self.suit}   |']
+                        grid+=[f'|     {self.suit}   {self.suit}     |']
+                        grid+=[f'|   {self.suit}   {self.suit}   {self.suit}   |']
+                    elif self.rank=='9':
+                        grid+=[f'|   {self.suit} {self.suit}   {self.suit} {self.suit}   |']
+                        grid+=[f'|       {self.suit}       |']
+                        grid+=[f'|   {self.suit} {self.suit}   {self.suit} {self.suit}   |']
+                    elif self.rank=='10':
+                        grid+=[f'|   {self.suit} {self.suit} {self.suit} {self.suit} {self.suit}   |']
+                        grid+=[f'|{" "*15}|']
+                        grid+=[f'|   {self.suit} {self.suit} {self.suit} {self.suit} {self.suit}   |']
+                    grid+=[f'|{self.rank}{" "*(14-len(self.rank))}{self.suit}|']
+                elif self.rank=='J':
+                    grid+=[f'|{self.suit}      ww     J|']
+                    grid+=[f'|       {{)      |']
+                    grid+=[f'|       {self.suit}       |']
+                    grid+=[f'|       {self.suit}       |']
+                    grid+=[f'|J     {self.suit}{self.suit}[     {self.suit}|']
+                elif self.rank=='Q':
+                    grid+=[f'|{self.suit}      ww     Q|']
+                    grid+=[f'|       {{(      |']
+                    grid+=[f'|       {self.suit}{self.suit}      |']
+                    grid+=[f'|      {self.suit}{self.suit}{self.suit}      |']
+                    grid+=[f'|Q     {self.suit}{self.suit}O     {self.suit}|']
+                elif self.rank=='K':
+                    grid+=[f'|{self.suit}      WW     K|']
+                    grid+=[f'|       {{)      |']
+                    grid+=[f'|       {self.suit}{self.suit}      |']
+                    grid+=[f'|      {self.suit}{self.suit}{self.suit}      |']
+                    grid+=[f'|K     {self.suit}{self.suit}>     {self.suit}|']
+            else:
+                grid+=[f'|{" "*15}|']
+                grid+=[f'|   +-------+   |']
+                grid+=[f'|   |   O   |   |']
+                grid+=[f'|   +-------+   |']
+                grid+=[f'|{" "*15}|']
+            grid+=[f'+{"-"*15}+']
+        return [list(line) for line in grid]
+
+    def getWidth(self):
+        return 11 if not self.isHorizontal else 9
+
+    def getHeight(self):
+        return 9 if not self.isHorizontal else 11
+
+    def copy(self):
+        return Card(self.rank,self.suit,self.x,self.y,self.isHorizontal)
+
+def newDeck():
+    return [Card(rank,suit) for suit in [SPADES,DIAMONDS,CLUBS,HEARTS] for rank in ['A','2','3','4','5','6','7','8','9','10','J','Q','K']]
 
 def printCards(hand):
-    width=os.get_terminal_size().columns
-    height=os.get_terminal_size().lines-2
-    grid=[[' ']*width for line in range(height)]
-    for card,pos in hand:
-        for row,line in enumerate(cardGrid(card)):
+    grid=[[' ']*WIDTH for line in range(HEIGHT)]
+    for card in hand:
+        for row,line in enumerate(card.getCardGrid()):
             for col,char in enumerate(line):
-                if 0<=row+pos[1]<height and 0<=col+pos[0]<width: # (x,y) is converted to (row,col) for printing
-                    grid[row+pos[1]][col+pos[0]]=char
+                if 0<=row+card.y<HEIGHT and 0<=col+card.x<WIDTH: # (x,y) is converted to (row,col) for printing
+                    grid[row+card.y][col+card.x]=char
+    out='\n'.join([''.join(line) for line in grid])
     os.system('cls' if os.name=='nt' else 'clear')
-    for line in grid:
-        print(''.join(line))
+    print(out)
 
-deck=newDeck()
-hand=[(('A',SPADES),(5,7)),(('5',HEARTS),(12,8))]
+hand=[]
+for index,card in enumerate((newDeck()[:13])):
+    a=card
+    b=a.copy()
+    a.y=index*a.getHeight()
+    b.x=a.getWidth()
+    b.y=index*a.getHeight()
+    b.isHorizontal=True
+    hand.append(a)
+    hand.append(b)
 printCards(hand)
